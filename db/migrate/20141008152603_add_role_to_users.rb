f29b0a3830bc0985f14a4553d9c80eb10afd8800
ErickFabian <fabianerickalfonso@gmail.com>
2014-10-14 14:08:21 -0500

    Finished layout changes

--- db/migrate/20141008152603_add_role_to_users.rb
+++ db/migrate/20141008152603_add_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :role_cd, :string, :default => :general
+    add_column :users, :role_cd, :string, default: :general
   end
 end
